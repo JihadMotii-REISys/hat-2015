5ef0fb3914a6880b5eacfb6e9f1e7cdced951a62
JihadMotii-REISys <jihad.motii@reisystems.com>
2015-11-03 20:32:15 -0500

    bugfix: path opt to install nodejs

--- scripts/provisioning/dev-dependencies.sh
+++ scripts/provisioning/dev-dependencies.sh
@@ -12,7 +12,7 @@ echo "Install Develpment Tools ..."
 yum -y groupinstall "Development tools"
 
 echo "Install NodeJS Latest stable ..."
-cd opt
+cd /opt
 wget http://nodejs.org/dist/v4.2.1/node-v4.2.1.tar.gz 
 tar zxf node-v4.2.1.tar.gz 
 cd node-v4.2.1 
