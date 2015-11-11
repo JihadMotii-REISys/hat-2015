23e4068c4cc29aed6f836a12154054041a0b1182
JihadMotii-REISys <jihad.motii@reisystems.com>
2015-11-10 19:50:56 -0500

    setup circle ci yml

--- scripts/docker/run-tests.sh
+++ scripts/docker/run-tests.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-#Build NPM and BOWER dependencies
-cd ./src/frontend/
-npm install
-bower install
-
 docker-compose -f docker-compose.test.yml up
 docker-compose -f docker-compose.test.yml stop
 docker-compose -f docker-compose.test.yml rm -f
