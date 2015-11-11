435b630cee4354a3746fcbbf7743f9f500d1ad2d
JihadMotii-REISys <jihad.motii@reisystems.com>
2015-11-10 23:38:48 -0500

    setup circle ci yml & karma & phantomJS

--- src/frontend/test/karma.conf.js
+++ src/frontend/test/karma.conf.js
@@ -75,6 +75,8 @@ module.exports = function(config) {
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_DEBUG,
+    
+    browserNoActivityTimeout: 100000,
 
     // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
